634ae84471186e69354487233038ef7f7e5a4fca
guptahani75@gmail.com <guptahani75@gmail.com>
2026-04-20 22:16:19 +0530

    add db file to server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const app = require('./src/app');
+const coonectDB = require('./src/config/database');
+
+coonectDB();
 
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000');
