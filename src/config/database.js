daa191f30bbe2c5a893c23bd36f577717e9552f4
guptahani75@gmail.com <guptahani75@gmail.com>
2026-04-21 22:14:03 +0530

    chnge the llines

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 async function connectDB(){
     try {
         await mongoose.connect(process.env.MONGO_URI);
-
         console.log("Connected to db!");
     } catch (error) {
         console.error("Error connecting to db:", error);
+
     }
 }
 
